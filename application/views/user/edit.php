70ff7b4fd7788305dbf72156f070fd4fe6dfa352
eriawan <eriawan@gti.co.id>
2022-07-24 10:31:21 +0700

    update

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -44,7 +44,8 @@
 
                       <div class="col-sm-5">
                         <?php
-                          echo cmb_dinamis('level_user', 'tbl_level_user', 'nama_level', 'id_level_user', $user['id_level_user']);
+                        $filter = ["id_level_user" => [1,6]];
+                          echo cmb_dinamis('level_user', 'tbl_level_user', 'nama_level', 'id_level_user', $user['id_level_user'], null, $filter);
                         ?>
                       </div>
                   </div>
